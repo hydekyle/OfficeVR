98dca2e0953ebc1e0c7f2f086f5697ab8b834c98
Ayoze Fernández <ayoze.fernandez@velocitech.io>
2022-10-04 11:39:49 +0100

    wp victor's office

--- Assets/1.Scripts/GameManager.cs
+++ Assets/1.Scripts/GameManager.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    class Pinga
-    {
 
-    }
-    void Start()
-    {
-        Pinga pinga = new();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
